088d012e4e965bf370bd6853f55a6ee16e43249c
Evergardener <henry0927@outlook.com>
2023-02-24 13:12:05 +0800

    Update transformer_keras.py

--- transformer_keras.py
+++ transformer_keras.py
@@ -1,64 +1,66 @@
 import tensorflow as tf
-from tensorflow import keras
 from tensorflow.keras import layers
 
-class MultiHeadSelfAttention(layers.Layer):
-    def __init__(self, embed_dim, num_heads):
-        super(MultiHeadSelfAttention, self).__init__()
-        self.embed_dim = embed_dim
-        self.num_heads = num_heads
-        if embed_dim % num_heads != 0:
-            raise ValueError(
-                f"Embedding dimension ({embed_dim}) should be divisible by number of heads ({num_heads})"
-            )
-        self.projection_dim = embed_dim // num_heads
-        self.query_dense = layers.Dense(embed_dim)
-        self.key_dense = layers.Dense(embed_dim)
-        self.value_dense = layers.Dense(embed_dim)
-        self.combine_heads = layers.Dense(embed_dim)
-
-    def attention(self, query, key, value):
-        score = tf.matmul(query, key, transpose_b=True)
-        dim_key = tf.cast(tf.shape(key)[-1], tf.float32)
-        scaled_score = score / tf.math.sqrt(dim_key)
-        weights = tf.nn.softmax(scaled_score, axis=-1)
-        output = tf.matmul(weights, value)
-        return output, weights
-
-    def separate_heads(self, x, batch_size):
-        x = tf.reshape(x, (batch_size, -1, self.num_heads, self.projection_dim))
+class MultiHeadAttention(layers.Layer):
+    def __init__(self, n_heads, d_model):
+        super(MultiHeadAttention, self).__init__()
+        self.n_heads = n_heads
+        self.d_model = d_model
+        
+        self.q_dense = layers.Dense(d_model)
+        self.k_dense = layers.Dense(d_model)
+        self.v_dense = layers.Dense(d_model)
+        
+        self.output_dense = layers.Dense(d_model)
+    
+    def split_heads(self, x, batch_size):
+        x = tf.reshape(x, (batch_size, -1, self.n_heads, self.d_model // self.n_heads))
         return tf.transpose(x, perm=[0, 2, 1, 3])
-
-    def call(self, inputs):
-        batch_size = tf.shape(inputs)[0]
-        query = self.query_dense(inputs)
-        key = self.key_dense(inputs)
-        value = self.value_dense(inputs)
-        query = self.separate_heads(query, batch_size)
-        key = self.separate_heads(key, batch_size)
-        value = self.separate_heads(value, batch_size)
-        attention, weights = self.attention(query, key, value)
-        attention = tf.transpose(attention, perm=[0, 2, 1, 3])
-        concat_attention = tf.reshape(attention, (batch_size, -1, self.embed_dim))
-        output = self.combine_heads(concat_attention)
-        return output
+    
+    def call(self, q, k, v, mask=None):
+        batch_size = tf.shape(q)[0]
+        
+        q = self.q_dense(q)
+        k = self.k_dense(k)
+        v = self.v_dense(v)
+        
+        q = self.split_heads(q, batch_size)
+        k = self.split_heads(k, batch_size)
+        v = self.split_heads(v, batch_size)
+        
+        scaled_dot_product = tf.matmul(q, k, transpose_b=True) / tf.math.sqrt(tf.dtypes.cast(self.d_model, tf.float32))
+        
+        if mask is not None:
+            scaled_dot_product += (mask * -1e9)
+        
+        attention_weights = tf.nn.softmax(scaled_dot_product, axis=-1)
+        
+        output = tf.matmul(attention_weights, v)
+        output = tf.transpose(output, perm=[0, 2, 1, 3])
+        output = tf.reshape(output, (batch_size, -1, self.d_model))
+        
+        return self.output_dense(output)
 
 class TransformerBlock(layers.Layer):
-    def __init__(self, embed_dim, num_heads, ff_dim, rate=0.1):
+    def __init__(self, n_heads, d_model, dff, dropout_rate):
         super(TransformerBlock, self).__init__()
-        self.att = MultiHeadSelfAttention(embed_dim, num_heads)
-        self.ffn = keras.Sequential(
-            [layers.Dense(ff_dim, activation="relu"), layers.Dense(embed_dim),]
-        )
+        self.multi_head_attention = MultiHeadAttention(n_heads, d_model)
+        self.ffn = tf.keras.Sequential([
+            layers.Dense(dff, activation='relu'),
+            layers.Dense(d_model)
+        ])
         self.layernorm1 = layers.LayerNormalization(epsilon=1e-6)
         self.layernorm2 = layers.LayerNormalization(epsilon=1e-6)
-        self.dropout1 = layers.Dropout(rate)
-        self.dropout2 = layers.Dropout(rate)
-
-    def call(self, inputs, training):
-        attn_output = self.att(inputs)
+        self.dropout1 = layers.Dropout(dropout_rate)
+        self.dropout2 = layers.Dropout(dropout_rate)
+    
+    def call(self, x, training, mask=None):
+        attn_output = self.multi_head_attention(x, x, x, mask)
         attn_output = self.dropout1(attn_output, training=training)
-        out1 = self.layernorm1(inputs + attn_output)
+        out1 = self.layernorm1(x + attn_output)
+        
         ffn_output = self.ffn(out1)
         ffn_output = self.dropout2(ffn_output, training=training)
-        return self.layernorm2(out1 + ffn_output)
+        out2 = self.layernorm2(out1 + ffn_output)
+        
+        return out2
